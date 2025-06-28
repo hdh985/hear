f9115ce21de1b760b1c50f6ca2893d78cc51840d
김경민 <hdh985@naver.com>
2025-06-28 04:57:06 -0700

    Initial commit

---
+++ src/main/java/kyunghee/hear/HearApplication.java
@@ -0,0 +1,13 @@
+package kyunghee.hear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearApplication.class, args);
+	}
+
+}
